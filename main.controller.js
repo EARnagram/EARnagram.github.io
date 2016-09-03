bb8b13f290c8fd8ea1767e45c6eb7fe1b809a9fe
Ezra Raez <ezRAez10@gmail.com>
2016-09-02 22:51:52 -0700

    remove projects array

--- main.controller.js
+++ main.controller.js
@@ -14,29 +14,29 @@
       $('.ui.sidebar').sidebar('toggle');
     }
 
-    vm.projects = [
-      {
-        name:         "Reversi",
-        hosted:       "http://ezraez.github.io/reversi",
-        github:       "https://github.com/ezRAez/reversi",
-        comment:      "The difficulty in programming Reversi is the dynamic \
-                       nature of the game play.  A move could take anywhere \
-                       between one and six opponent pieces in a given move."
-        codeImage:    "../assets/images/projects/reversi_code.png",
-        codeComment:  "<code>searchBoard</code> compiles the surrounding \
-                       squares' values in a given direction into a \
-                       string, making it easy for a regex to find a \
-                       valid move.",
-        projectImage: "../assets/images/projects/reversi.png",
-        techniques:   [
-                        "Recursive Function Calls",
-                        "Complex Logic",
-                        "Model/View Separation",
-                        "DOM manipulation with jQuery",
-                        "Regex for Move Validation"
-                      ]
+    // vm.projects = [
+    //   {
+    //     name:         "Reversi",
+    //     hosted:       "http://ezraez.github.io/reversi",
+    //     github:       "https://github.com/ezRAez/reversi",
+    //     comment:      "The difficulty in programming Reversi is the dynamic \
+    //                    nature of the game play.  A move could take anywhere \
+    //                    between one and six opponent pieces in a given move."
+    //     codeImage:    "../assets/images/projects/reversi_code.png",
+    //     codeComment:  "<code>searchBoard</code> compiles the surrounding \
+    //                    squares' values in a given direction into a \
+    //                    string, making it easy for a regex to find a \
+    //                    valid move.",
+    //     projectImage: "../assets/images/projects/reversi.png",
+    //     techniques:   [
+    //                     "Recursive Function Calls",
+    //                     "Complex Logic",
+    //                     "Model/View Separation",
+    //                     "DOM manipulation with jQuery",
+    //                     "Regex for Move Validation"
+    //                   ]
 
-      }
-    ];
+    //   }
+    // ];
   }
 })();
